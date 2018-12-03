41830c811971536f77f57ff43853407ebedfe79a
Mark Reid <mindmark@gmail.com>
2018-12-02 18:54:45 -0800

    added misc properties

--- avb/misc.py
+++ avb/misc.py
@@ -30,6 +30,10 @@ from . utils import (
 @utils.register_class
 class FileLocator(core.AVBObject):
     class_id = b'FILE'
+    properties = [
+        AVBProperty('path_name', 'OMFI:FL:PathName', 'string'),
+        AVBProperty('paths',     'OMFI:FL:Paths',     'list'),
+    ]
 
     def read(self, f):
         super(FileLocator, self).read(f)
@@ -41,36 +45,34 @@ class FileLocator(core.AVBObject):
         assert version == 2
         self.paths = []
         path = read_string(f)
-
         if path:
-            self.paths.append(path)
-            tag = read_byte(f)
-            version = read_byte(f)
-            assert tag == 0x01
-            assert version == 1
-            tag = read_byte(f)
-            assert tag == 76
-            path = read_string(f)
-            self.paths.append(path)
+            self.path_name= path
 
-            tag = read_byte(f)
-            version = read_byte(f)
+        for tag in iter_ext(f):
+            if tag == 0x01:
+                read_assert_tag(f, 76)
+                path = read_string(f)
+                if path:
+                    self.paths.append(path)
 
-            assert tag == 0x01
-            assert version == 2
-            tag = read_byte(f)
-            assert tag == 76
-            path = read_string(f)
-            self.paths.append(path)
+            elif tag == 0x02:
+                read_assert_tag(f, 76)
+                # utf-8?
+                path = read_string(f)
+                if path:
+                    self.paths.append(path)
+            else:
+                raise ValueError("%s: unknown ext tag 0x%02X %d" % (str(self.class_id), tag,tag))
 
-        # end tag
         tag = read_byte(f)
         assert tag == 0x03
 
 @utils.register_class
 class GraphicEffect(core.AVBObject):
     class_id = b'GRFX'
-
+    properties = [
+        AVBProperty('pict_data', 'OMFI:MC:GRFX:PictData', 'bytes'),
+    ]
     def read(self, f):
 
         read_assert_tag(f, 0x02)
