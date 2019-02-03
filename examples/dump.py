7d8df669a4dae58a8377f4949f3da2a24d869bf3
Mark Reid <mindmark@gmail.com>
2019-02-02 19:22:42 -0800

    remove unused code

--- examples/dump.py
+++ examples/dump.py
@@ -7,33 +7,6 @@ from __future__ import (
 import avb
 import sys
 
-
-# def dump(item, space=''):
-#
-#
-#     if isinstance(item, avb.utils.AVBObjectRef):
-#         dump(item.value, space)
-#         return
-#
-#     if not isinstance(item, avb.core.AVBObject):
-#         if item is None:
-#             return
-#         s = str(item)
-#         if s:
-#             print space, s
-#         return
-#
-#     for name, value in item.properties():
-#         print space, name, type(value), ':',
-#         if isinstance(value, list):
-#             print 'len:', len(value)
-#             for obj in value:
-#                 dump(obj, space + " ")
-#         else:
-#             print "";
-#             dump(value, space + "  ")
-#
-
 if bytes is not str:
     unicode = str
 
@@ -42,30 +15,6 @@ def pretty_value(value):
         return "bytearray(%d)" % len(value)
         # return ''.join(format(x, '02x') for x in value)
     return value
-
-def dump_obj(obj):
-
-    for pdef in obj.propertydefs:
-        key = pdef.name
-        if key in obj.property_data:
-
-            value = obj.property_data[key]
-            if isinstance(value, dict):
-                print("%s:" % key)
-                for sub_key, sub_value in value.items():
-                    print("  ", "%s:" % sub_key, unicode(sub_value))
-
-            elif isinstance(value, list):
-                print("%s:" % key)
-                for sub_value in value:
-                    if isinstance(sub_value, avb.trackgroups.Track):
-                        for ref in sub_value.refs:
-                            print("     ", type(ref.value), ref.value )
-                    else:
-                        print("  ", sub_value)
-
-            else:
-                print("%s:" % key , obj.property_data[key])
 
 def dump(obj, space=""):
 
