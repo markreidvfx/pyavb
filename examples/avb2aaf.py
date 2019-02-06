513ef08003aa931fe7b8464f443b6c8f4038fc8c
Mark Reid <mindmark@gmail.com>
2019-02-05 19:35:26 -0800

    initial retime convert

--- examples/avb2aaf.py
+++ examples/avb2aaf.py
@@ -7,7 +7,9 @@ from __future__ import (
 
 import avb
 import aaf2
+from aaf2.rational import AAFRational
 import sys
+from uuid import UUID
 
 def register_definitions(f):
     op_def = f.create.OperationDef('89d9b67e-5584-302d-9abd-8bd330c46841', 'VideoDissolve_2', '')
@@ -34,6 +36,12 @@ def register_definitions(f):
     linear = f.create.InterpolationDef('5b6c85a4-0ede-11d3-80a9-006008143e6f', 'LinearInterp', '')
     f.dictionary.register_def(linear)
 
+    cubic = f.create.InterpolationDef('a04a5439-8a0e-4cb7-975f-a5b255866883', 'AvidCubicInterpolator', '')
+    f.dictionary.register_def(cubic)
+
+    constant = f.create.InterpolationDef('5b6c85a5-0ede-11d3-80a9-006008143e6f', 'ConstantInterp', '')
+    f.dictionary.register_def(constant)
+
     op_def = f.create.OperationDef('0c3bea41-fc05-11d2-8a29-0050040ef7d2', 'Audio Dissolve', '')
     f.dictionary.register_def(op_def)
 
@@ -43,6 +51,59 @@ def register_definitions(f):
     op_def['NumberInputs'].value = 2
     op_def['OperationCategory'].value = 'OperationCategory_Effect'
     op_def.parameters.extend([param_byteorder, param_effect_id])
+
+    op_def = f.create.OperationDef('9d2ea890-0968-11d3-8a38-0050040ef7d2', 'Motion Control', '')
+    f.dictionary.register_def(op_def)
+
+    op_def.media_kind = 'picture'
+    op_def['IsTimeWarp'].value = True
+    op_def['Bypass'].value = 0
+    op_def['NumberInputs'].value = 1
+    op_def['OperationCategory'].value = 'OperationCategory_Effect'
+
+
+    param = f.create.ParameterDef("f7ffed29-fc8e-43ed-943a-4b57b5c157ee", "AvidMotionPulldown", "", 'aafInt32')
+    f.dictionary.register_def(param)
+    op_def.parameters.append(param)
+
+    param = f.create.ParameterDef("8dde1839-6862-4874-a1e9-4fbd1164f22a", "AvidMotionOutputFormat", "", 'aafInt32')
+    f.dictionary.register_def(param)
+    op_def.parameters.append(param)
+
+    param = f.create.ParameterDef("5b22ff71-c51b-11d3-a069-006094eb75cb", "AvidPhase", "", 'aafInt32')
+    f.dictionary.register_def(param)
+    op_def.parameters.append(param)
+
+    param = f.create.ParameterDef("72559a80-24d7-11d3-8a50-0050040ef7d2", "SpeedRatio", "", 'Rational')
+    f.dictionary.register_def(param)
+    op_def.parameters.append(param)
+
+    param = f.create.ParameterDef("1c5a02d5-e503-4ca6-8617-d7914bb8ac03", "AvidMotionInputFormat", "", 'Rational')
+    f.dictionary.register_def(param)
+    op_def.parameters.append(param)
+
+    # used on Motion Control but not part of op_def
+    param = f.create.ParameterDef("8d56827c-847e-11d5-935a-50f857c10000", "PARAM_SPEED_MAP_U", "", 'Rational')
+    f.dictionary.register_def(param)
+
+    param = f.create.ParameterDef("8d56827d-847e-11d5-935a-50f857c10000", "PARAM_SPEED_OFFSET_MAP_U", "", 'Rational')
+    f.dictionary.register_def(param)
+
+    param = f.create.ParameterDef("8d568283-847e-11d5-935a-50f857c10000", "PARAM_PULLDOWN_PHASE_U", "", 'Rational')
+    f.dictionary.register_def(param)
+
+    param = f.create.ParameterDef('8d56830e-847e-11d5-935a-50f857c10000', 'PP_IN_TANGENT_POS_U',  '', 'Rational')
+    f.dictionary.register_def(param)
+    param = f.create.ParameterDef('8d56830f-847e-11d5-935a-50f857c10000', 'PP_IN_TANGENT_VAL_U',  '', 'Rational')
+    f.dictionary.register_def(param)
+    param = f.create.ParameterDef('8d568310-847e-11d5-935a-50f857c10000', 'PP_OUT_TANGENT_POS_U', '', 'Rational')
+    f.dictionary.register_def(param)
+    param = f.create.ParameterDef('8d568311-847e-11d5-935a-50f857c10000', 'PP_OUT_TANGENT_VAL_U', '', 'Rational')
+    f.dictionary.register_def(param)
+    param = f.create.ParameterDef('8d568312-847e-11d5-935a-50f857c10000', 'PP_TANGENT_MODE_U',    '', 'Rational')
+    f.dictionary.register_def(param)
+    param = f.create.ParameterDef('8d568313-847e-11d5-935a-50f857c10000', 'PP_BASE_FRAME_U',      '', 'Rational')
+    f.dictionary.register_def(param)
 
 def nice_edit_rate(rate):
     if rate == 24:
@@ -273,10 +334,91 @@ def convert_essence_group(f, avb_essence_group):
 
     return e
 
+interp_kinds = {
+2: 'ConstantInterp',
+3: 'LinearInterp',
+6: 'AvidCubicInterpolator',
+}
+
+extrap_kinds = {
+1: UUID('0e24dd54-66cd-4f1a-b0a0-670ac3a7a0b3'),
+}
+
+pp_codes = {
+5:  'PP_IN_TANGENT_POS_U',
+6:  'PP_IN_TANGENT_VAL_U',
+7:  'PP_OUT_TANGENT_POS_U',
+8:  'PP_OUT_TANGENT_VAL_U',
+9:  'PP_TANGENT_MODE_U',
+14: 'PP_BASE_FRAME_U',
+}
+
+def convert_parameter(f, name, param):
+    interpdef = interp_kinds[param.control_track.interp_kind]
+    if interpdef in ('ConstantInterp', ):
+        assert len(param.control_track.control_points) == 1
+        p = param.control_track.control_points[0]
+        var = f.create.ConstantValue(name, p.value)
+        return var
+
+    var = f.create.VaryingValue(name, interpdef)
+    var['VVal_Extrapolation'].value = extrap_kinds[param.control_track.extrap_kind]
+    var['VVal_FieldCount'].value = 1
+
+    for p in param.control_track.control_points:
+        t = AAFRational(p.offset[0], p.offset[1])
+        point = var.add_keyframe(t, p.value, 'RelativeFixed')
+        bezier_controls = []
+        for pp in p.pp:
+            pp_control = f.create.ConstantValue(pp_codes[pp.code], pp.value)
+            bezier_controls.append(pp_control)
+
+        point['ControlPointPointProperties'].extend(bezier_controls)
+
+            # print(pp_codes[pp.code], pp_control)
+        # print(t, p.value, point)
+    return var
+
+
 def convert_motion_effect(f, avb_effect):
+    # avb_dump(avb_effect)
+
+    op_group = f.create.OperationGroup("Motion Control")
+
+    for param in avb_effect.param_list:
+        # avb_dump(param)
+
+        var = None
+        if param.uuid == UUID("8d56827c-847e-11d5-935a-50f857c10000"):
+            var = convert_parameter(f, 'PARAM_SPEED_MAP_U', param)
+        elif param.uuid == UUID("8d56827d-847e-11d5-935a-50f857c10000"):
+            var = convert_parameter(f, 'PARAM_SPEED_OFFSET_MAP_U', param)
+        # elif param.uuid == UUID("8d568283-847e-11d5-935a-50f857c10000"):
+        #     var = convert_parameter(f, "PARAM_PULLDOWN_PHASE_U", param)
+        # # else:
+        #     avb_dump(avb_effect)
+        if var:
+            op_group.parameters.append(var)
+
     track = avb_effect.tracks[0]
-    components = list(track.component.components)
-    return convert_component(f, components[1])
+    ratio = avb_effect.speed_ratio
+    param = f.create.ConstantValue("SpeedRatio", AAFRational(ratio[0], ratio[1]))
+    op_group.parameters.append(param)
+
+    attr = avb_effect.attributes.get('_MFX_INPUT_FORMAT', None)
+    if attr is not None:
+        param = f.create.ConstantValue('AvidMotionInputFormat', attr)
+        op_group.parameters.append(param)
+
+    attr = avb_effect.attributes.get('_MFX_OUTPUT_FORMAT', None)
+    if attr is not None:
+        param = f.create.ConstantValue('AvidMotionOutputFormat', attr)
+        op_group.parameters.append(param)
+
+    segment = convert_component(f, track.component)
+    op_group['InputSegments'].append(segment)
+
+    return op_group
 
 def convert_component(f, avb_component):
 
@@ -327,15 +469,17 @@ def convert_slots(aaf_file, comp, aaf_mob):
 
     slot_id = 1
     for track in comp.tracks:
-        # if not track.component.media_kind in ('picture', 'sound'):
-            # continue
+        print(track)
+        if track.component.media_kind in ('picture', 'sound', 'timecode', 'edgecode'):
+            slot = aaf_file.create.TimelineMobSlot()
 
-        # if
+            # raise Exception()
+        elif track.component.media_kind in ('DescriptiveMetadata', ):
+            slot = aaf_file.create.EventMobSlot()
+        else:
+            raise Exception("Unknown media_kind: %s" % track.component.media_kind)
 
-        # print(track, track.component)
-        # print(track.segment.media_kind)
 
-        slot = aaf_file.create.TimelineMobSlot()
         slot.edit_rate = track.component.edit_rate
         slot.segment = convert_component(aaf_file, track.component)
         slot.slot_id = track.index
@@ -344,6 +488,10 @@ def convert_slots(aaf_file, comp, aaf_mob):
         slot_id += 1
 
 def convert_composition(f, avb_mob):
+    mob_id = avb_mob.mob_id
+    if mob_id in f.content.mobs:
+        return f.content.mobs.get(mob_id)
+
     if avb_mob.mob_type == 'MasterMob':
         aaf_mob = f.create.MasterMob()
         aaf_mob.mob_id = avb_mob.mob_id
