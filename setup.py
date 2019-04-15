5bc60101449b49e0a485f1a0b600899492103d64
Mark Reid <mindmark@gmail.com>
2019-04-14 21:31:17 -0700

    update to dev2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 import setuptools.command.build_py
 
 PROJECT_METADATA = {
-    "version": "0.1.0.dev",
+    "version": "0.1.0.dev2",
     "author": 'Mark Reid',
     "author_email": 'mindmark@gmail.com',
     "license": 'MIT',
