ea2bfd4985c0fc35f8e416cfbed56845d8543b7c
Mark Reid <mindmark@gmail.com>
2022-06-20 15:43:08 -0700

    skip linux wheels

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools.command.build_py
 from distutils.extension import Extension
 
 PROJECT_METADATA = {
-    "version": "0.1.0.dev4",
+    "version": "0.1.0.dev5",
     "author": 'Mark Reid',
     "author_email": 'mindmark@gmail.com',
     "license": 'MIT',
