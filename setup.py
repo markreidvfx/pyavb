31b474184782658ceacab6603a7ec7d786d4b473
Mark Reid <mindmark@gmail.com>
2022-06-30 00:38:14 -0700

    1.1.0 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools.command.build_py
 from distutils.extension import Extension
 
 PROJECT_METADATA = {
-    "version": "1.1.0.dev1",
+    "version": "1.1.0",
     "author": 'Mark Reid',
     "author_email": 'mindmark@gmail.com',
     "license": 'MIT',
