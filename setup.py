da8777950abcef4f6063c61206f0ae7cdf2dfdb0
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2022-07-01 18:15:31 -0400

    Remove Python 3.5 and 3.6 from classifiers

--- setup.py
+++ setup.py
@@ -79,8 +79,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -100,6 +98,7 @@ setup(
     cmdclass={'build_py': AddMetadata},
     ext_modules = extensions,
     extras_require= {'cython' : ['cython']},
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*',
 
     **PROJECT_METADATA
 )
